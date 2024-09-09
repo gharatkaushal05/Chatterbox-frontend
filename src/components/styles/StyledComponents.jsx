1cde05fd35afbdac76090f488a84186dc982aab6
gharatkaushal05 <gharatkaushal05@gmail.com>
2024-09-09 17:43:59 +0530

    refactor

--- src/components/styles/StyledComponents.jsx
+++ src/components/styles/StyledComponents.jsx
@@ -1,63 +1,62 @@
-import React from 'react'
+import React from "react";
 import { keyframes, Skeleton, styled } from "@mui/material";
-import {Link as LinkComponent} from "react-router-dom"
-import { grayColor, matteBlack } from '../../constants/color';
+import { Link as LinkComponent } from "react-router-dom";
+import { grayColor, matteBlack } from "../../constants/color";
 
 export const VisuallyHiddenInput = styled("input")({
-    border:0,
-    clip: "rect(0 0 0 0)",
-    height:1,
-    margin: -1,
-    overflow:"hidden",
-    padding: 0,
-    position: "absolute",
-    whiteSpace: "nowrap",
-    width:1
-})
+  border: 0,
+  clip: "rect(0 0 0 0)",
+  height: 1,
+  margin: -1,
+  overflow: "hidden",
+  padding: 0,
+  position: "absolute",
+  whiteSpace: "nowrap",
+  width: 1,
+});
 
 export const Link = styled(LinkComponent)`
-text-decoration: none;
-color: black;
-padding:1rem;
-&:hover{
-background-color: rgba(0,0,0,0.1);
-}
+  text-decoration: none;
+  color: black;
+  padding: 1rem;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
 `;
 
-
 export const InputBox = styled("input")`
-width: 100%;
-height: 100%;
-border: none;
-outline: none;
-padding: 0 3rem;
-border-radius: 1.5rem;
-background-color: ${grayColor};
-
-`
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  padding: 0 3rem;
+  border-radius: 1.5rem;
+  background-color: ${grayColor};
+`;
 
 export const SearchField = styled("input")`
-padding: 1rem 2rem;
-width: 20vmax;
-border: none;
-outline: none;
-border-radius: 1.5rem;
-background-color: #f1f1f1;
-font-size: 1.1rem
-`
+  padding: 1rem 2rem;
+  width: 20vmax;
+  border: none;
+  outline: none;
+  border-radius: 1.5rem;
+  background-color: #f1f1f1;
+  font-size: 1.1rem;
+`;
 
 export const CurveButton = styled("button")`
-border-radius: 1.5rem;
-padding: 1rem 2rem;
-border: none;
-outline: none;
-cursor: pointer;
-background-color: ${matteBlack};
-color: white;
-font-size: 1.1rem;
-&:hover{
-    background-color: rgba(0,0,0,0.8)
-}`
+  border-radius: 1.5rem;
+  padding: 1rem 2rem;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  background-color: ${matteBlack};
+  color: white;
+  font-size: 1.1rem;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+`;
 
 const bounceAnimation = keyframes`
 0% {transform: scale(1);}
@@ -65,6 +64,6 @@ const bounceAnimation = keyframes`
 100% {transform: scale(1);}
 `;
 
-export const BouncingSkeleton = styled(Skeleton)(()=>({
-    animation: `${bounceAnimation} 1s infinite`,
-}))
+export const BouncingSkeleton = styled(Skeleton)(() => ({
+  animation: `${bounceAnimation} 1s infinite`,
+}));
